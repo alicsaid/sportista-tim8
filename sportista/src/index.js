36dd80ac8bcf5c894e7c641a0594feaacdd4ad74
almababic <117942503+almababic@users.noreply.github.com>
2023-04-28 22:47:10 +0200

    Add files via upload

--- sportista/src/index.js
+++ sportista/src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'; // ovo importa bootstrap
 
+import LoginForm from "./components/login/LoginForm";
+import RegisterForm from "./components/register/RegisterForm";
+import Users from "./pages/admin/Users";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RegisterForm />
   </React.StrictMode>
 );
