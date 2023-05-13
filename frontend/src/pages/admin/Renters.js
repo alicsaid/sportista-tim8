0973cc1549f7968513cb582d04391a6d0a262a7e
almababic <117942503+almababic@users.noreply.github.com>
2023-05-13 13:54:55 +0200

    Added more sports to table of renters and users on admin page

--- frontend/src/pages/admin/Renters.js
+++ frontend/src/pages/admin/Renters.js
@@ -1,60 +1,183 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import Dropdown from 'react-bootstrap/Dropdown';
-import "./Admin.css";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import "./Renters.css";
 
 //components
 import Sidebar from "../../components/navigation/Sidebar";
 
 function Renters() {
-    const renters = [
-        { id: 1, name: "Vistafon", address: "Bulevar Meše Selimovića bb, Sarajevo", email: "vistafon@example.com", sports: ["Football", "Handball"] },
-        { id: 2, name: "Fudbalski tereni Bentbaša", address: "Aleja Ambasadora bb, Sarajevo", email: "bentbasa@example.com", sports: ["Football"] },
-        { id: 3, name: "Zetra", address: "Alipašina bb, Sarajevo", email: "zetra@example.com", sports: ["Football", "Tennis"] },
-        { id: 4, name: "Centar Skenderija", address: "Terezija bb, Sarajevo", email: "skenderija@example.com", sports: ["Football", "Basketball", "Volleyball"] },
-        { id: 5, name: "Centar Safet Zajko", address: "Halilovići bb, Sarajevo", email: "safetzajko@example.com", sports: ["Football", "Tennis", "Volleyball", "Basketball"] },
-    ];
+    const [renters, setRenters] = useState([
+        {
+            id: 1,
+            name: "renter1",
+            email: "renter1@example.com",
+            city: "Sarajevo",
+            phone: "062123123",
+            sports: ["Football", "Handball", "Volleyball", "Tennis"]
+        },
+        {
+            id: 2,
+            name: "renter2",
+            email: "renter2@example.com",
+            city: "Sarajevo",
+            phone: "062123124",
+            sports: ["Paintball", "Table tennis", "Airsoft", "Boxing", "Hockey"]
+        },
+        {
+            id: 3,
+            name: "renter3",
+            email: "renter3@example.com",
+            city: "Mostar",
+            phone: "062123125",
+            sports: ["Football", "Volleyball", "Handball", "Basketball"]
+        },
+        {
+            id: 4,
+            name: "renter4",
+            email: "renter4@example.com",
+            city: "Konjic",
+            phone: "062123126",
+            sports: ["Table tennis", "Volleyball", "Tennis", "Airsoft"]
+        },
+        {
+            id: 5,
+            name: "renter5",
+            email: "renter5@example.com",
+            city: "Banja Luka",
+            phone: "062123127",
+            sports: ["Ice skating", "Paintball", "Football"]
+        },
+        {
+            id: 6,
+            name: "renter6",
+            email: "renter6@example.com",
+            city: "Travnik",
+            phone: "062123128",
+            sports: ["Volleyball", "Boxing", "Handball", "Paintball"]
+        },
+        {
+            id: 7,
+            name: "renter7",
+            email: "renter7@example.com",
+            city: "Sarajevo",
+            phone: "062123129",
+            sports: ["Basketball", "Football", "Volleyball", "Hockey"]
+        },
+        {
+            id: 8,
+            name: "renter8",
+            email: "renter8@example.com",
+            city: "Mostar",
+            phone: "062123120",
+            sports: ["Table tennis", "Volleyball", "Football"]
+        },
+
+    ]);
+
+    const [selectedRenter, setSelectedRenter] = useState(null);
+
+    const handleDelete = (id) => {
+        const updatedRenters = renters.filter(renter => renter.id !== id);
+
+        const updatedRentersWithNewIds = updatedRenters.map((renter, index) => ({
+            ...renter,
+            id: index + 1,
+        }));
+
+        setRenters(updatedRentersWithNewIds);
+    };
+
+    const handleViewDetails = (id) => {
+        const renter = renters.find(renter => renter.id === id);
+        setSelectedRenter(renter);
+    };
+
+    const handleCloseModal = () => {
+        setSelectedRenter(null);
+    };
 
     return (
-        <div style={{ display: 'flex' }}>
-            <Sidebar />
-            <div style={{ marginLeft: '27rem', marginTop: '2rem' }}>
+        <div style={{display: 'flex'}}>
+            <Sidebar/>
+            <div style={{marginLeft: '27rem', marginTop: '2rem', marginBottom: '2rem', marginRight: '1rem', height: 'calc(100vh - 5rem)', overflowY: 'auto' }}>
                 <h1>Renters</h1>
 
                 <div className="table">
-                    <Table striped bordered hover size="sm" style={{border: '3px solid #ccc',  boxShadow: '0 0 5px rgba(0,0,0,0.2)'}}>
+                    <Table striped bordered hover size="sm"
+                           style={{border: '3px solid #ccc', boxShadow: '0 0 5px rgba(0,0,0,0.2)'}}>
                         <thead>
-                            <tr>
-                                <th className="table-content">ID</th>
-                                <th className="table-content">Name of sports center</th>
-                                <th className="table-content">Address</th>
-                                <th className="table-content">Email address</th>
-                                <th className="table-content">Available sports</th>
-                            </tr>
+                        <tr>
+                            <th className="table-content">ID</th>
+                            <th className="table-content">Name</th>
+                            <th className="table-content">Email address</th>
+                            <th className="table-content">City</th>
+                            <th className="table-content">Phone number</th>
+                            <th className="table-content">Available sports</th>
+                            <th className="table-content">Actions</th>
+                        </tr>
                         </thead>
                         <tbody>
-                            {renters.map(renter => (
-                                <tr key={renter.id}>
+                        {renters.map(renter => (
+                            <React.Fragment key={renter.id}>
+                                <tr>
                                     <td className="table-content">{renter.id}</td>
                                     <td className="table-content">{renter.name}</td>
-                                    <td className="table-content">{renter.address}</td>
                                     <td className="table-content">{renter.email}</td>
+                                    <td className="table-content">{renter.city}</td>
+                                    <td className="table-content">{renter.phone}</td>
                                     <td className="table-content">
                                         {renter.sports.length === 1
                                             ? renter.sports[0]
-                                            : <Dropdown>
-                                                <Dropdown.Toggle variant="success" style={{background: 'none', border: 'none', color: 'black'}}>
-                                                </Dropdown.Toggle>
-                                                <Dropdown.Menu>
-                                                    {renter.sports.map(sport => (
-                                                        <Dropdown.Item key={sport}>{sport}</Dropdown.Item>
-                                                    ))}
-                                                </Dropdown.Menu>
-                                            </Dropdown>
+                                            : (
+                                                <Dropdown>
+                                                    <Dropdown.Toggle variant="success" style={{
+                                                        background: 'none',
+                                                        border: 'none',
+                                                        color: 'black'
+                                                    }}>
+                                                    </Dropdown.Toggle>
+                                                    <Dropdown.Menu>
+                                                        {renter.sports.map(sport => (
+                                                            <Dropdown.Item key={sport}>{sport}</Dropdown.Item>
+                                                        ))}
+                                                    </Dropdown.Menu>
+                                                </Dropdown>
+                                            )
                                         }
                                     </td>
+                                    <td className="table-content">
+                                        <div className="button-group">
+                                            <Button className="buttonView" size="sm" onClick={() => handleViewDetails(renter.id)}>View</Button>
+                                            <Button className="buttonDelete" variant="danger" size="sm"
+                                                    onClick={() => handleDelete(renter.id)}>Delete</Button>
+                                        </div>
+                                    </td>
                                 </tr>
-                            ))}
+                                {selectedRenter && selectedRenter.id === renter.id && (
+                                    <tr>
+                                        <td colSpan={6}>
+                                            <Modal show={true} onHide={handleCloseModal} centered>
+                                                <Modal.Header closeButton>
+                                                    <Modal.Title>Renter Details</Modal.Title>
+                                                </Modal.Header>
+                                                <Modal.Body>
+                                                    <p><strong>ID:</strong> {selectedRenter.id}</p>
+                                                    <p><strong>Name:</strong> {selectedRenter.name}</p>
+                                                    <p><strong>Email:</strong> {selectedRenter.email}</p>
+                                                    <p><strong>City:</strong> {selectedRenter.city}</p>
+                                                    <p><strong>Phone number:</strong> {selectedRenter.phone}</p>
+                                                    <p><strong>Available
+                                                        Sports:</strong> {selectedRenter.sports.join(", ")}</p>
+                                                </Modal.Body>
+                                            </Modal>
+                                        </td>
+                                    </tr>
+                                )}
+                            </React.Fragment>
+                        ))}
                         </tbody>
                     </Table>
                 </div>
--- frontend/src/pages/admin/Users.js
+++ frontend/src/pages/admin/Users.js
@@ -1,41 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import Dropdown from 'react-bootstrap/Dropdown';
-import "./Admin.css";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import "./Users.css";
 
 //components
 import Sidebar from "../../components/navigation/Sidebar";
 
 function Users() {
-    const users = [
-        { id: 1, firstName: "John", lastName: "Doe", gender: "Male", dateOfBirth: "13-04-1995", email: "john@example.com", sports: ["Football", "Basketball", "Handball"] },
-        { id: 2, firstName: "Jennifer", lastName: "Smith", gender: "Female", dateOfBirth: "05-05-1999", email: "jennifer@example.com", sports: ["Volleyball", "Basketball", "Tennis"] },
-        { id: 3, firstName: "Mike", lastName: "Smith", gender: "Male", dateOfBirth: "29-04-1993", email: "mike@example.com", sports: ["Football", "Basketball", "Volleyball"] },
-        { id: 4, firstName: "Jane", lastName: "Roe", gender: "Female", dateOfBirth: "02-07-1997", email: "jane@example.com", sports: ["Tennis"] },
-        { id: 5, firstName: "Sarah", lastName: "Campbell", gender: "Female", dateOfBirth: "07-08-2001", email: "sarah@example.com", sports: ["Basketball", "Handball"] },
-        { id: 6, firstName: "Tom", lastName: "Wade", gender: "Male", dateOfBirth: "21-03-2000", email: "tom@example.com", sports: ["Football"] },
-        { id: 7, firstName: "Austin", lastName: "Toe", gender: "Male", dateOfBirth: "25-09-1996", email: "austin@example.com", sports: ["Tennis", "Basketball"] },
-        { id: 8, firstName: "Edward", lastName: "Stiles", gender: "Male", dateOfBirth: "27-12-1999", email: "edward@example.com", sports: ["Basketball"] },
-        { id: 9, firstName: "Mia", lastName: "Soler", gender: "Female", dateOfBirth: "09-03-2004", email: "mia@example.com", sports: ["Volleyball", "Football"] },
-    ];
+    const [users, setUsers] = useState([
+        { id: 1, firstName: "John", lastName: "Doe", email: "john@example.com", gender: "Male", dateOfBirth: "13-04-1995", sports: ["Football", "Basketball", "Handball", "Paintball"] },
+        { id: 2, firstName: "Jennifer", lastName: "Smith", email: "jennifer@example.com", gender: "Female", dateOfBirth: "05-05-1999", sports: ["Volleyball", "Basketball", "Tennis", "Ice skating"] },
+        { id: 3, firstName: "Mike", lastName: "Smith", email: "mike@example.com", gender: "Male", dateOfBirth: "29-04-1993", sports: ["Football", "Basketball", "Volleyball", "Boxing"] },
+        { id: 4, firstName: "Jane", lastName: "Roe", email: "jane@example.com", gender: "Female", dateOfBirth: "02-07-1997", sports: ["Tennis", "Table tennis", "Ice skating"] },
+        { id: 5, firstName: "Sarah", lastName: "Campbell", email: "sarah@example.com", gender: "Female", dateOfBirth: "07-08-2001", sports: ["Basketball", "Handball", "Table tennis"] },
+        { id: 6, firstName: "Tom", lastName: "Wade", email: "tom@example.com", gender: "Male", dateOfBirth: "21-03-2000", sports: ["Football", "Hockey", "Airsoft"] },
+        { id: 7, firstName: "Austin", lastName: "Toe", email: "austin@example.com", gender: "Male", dateOfBirth: "25-09-1996", sports: ["Tennis", "Basketball", "Airsoft"] },
+        { id: 8, firstName: "Edward", lastName: "Stiles", email: "edward@example.com", gender: "Male", dateOfBirth: "27-12-1999", sports: ["Basketball", "Boxing", "Paintball"] },
+        { id: 9, firstName: "Mia", lastName: "Soler", email: "mia@example.com", gender: "Female", dateOfBirth: "09-03-2004", sports: ["Volleyball", "Football", "Hockey"] },
+    ]);
+
+    const [showModal, setShowModal] = useState(false);
+    const [selectedUser, setSelectedUser] = useState(null);
+
+    const handleDelete = (id) => {
+        const updatedUsers = users.filter(user => user.id !== id);
+
+        const updatedUsersWithNewIds = updatedUsers.map((user, index) => ({
+            ...user,
+            id: index + 1,
+        }));
+
+        setUsers(updatedUsersWithNewIds);
+    };
+
+    const handleOpenModal = (user) => {
+        setSelectedUser(user);
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setSelectedUser(null);
+        setShowModal(false);
+    };
 
     return (
         <div style={{ display: 'flex' }}>
             <Sidebar />
-            <div style={{ marginLeft: '27rem', marginTop: '2rem' }}>
+            <div style={{ marginLeft: '27rem', marginTop: '2rem', marginBottom: '2rem', marginRight: '1rem', height: 'calc(100vh - 5rem)', overflowY: 'auto' }}>
                 <h1>Users</h1>
 
                 <div className="table">
-                    <Table striped bordered hover size="sm" style={{border: '3px solid #ccc',  boxShadow: '0 0 5px rgba(0,0,0,0.2)'}}>
+                    <Table striped bordered hover size="sm" style={{border: '3px solid #ccc', boxShadow: '0 0 5px rgba(0,0,0,0.2)'}}>
                         <thead>
                         <tr>
                             <th className="table-content">ID</th>
                             <th className="table-content">First name</th>
                             <th className="table-content">Last name</th>
+                            <th className="table-content">Email address</th>
                             <th className="table-content">Gender</th>
                             <th className="table-content">Date of birth</th>
-                            <th className="table-content">Email address</th>
                             <th className="table-content">Sports</th>
+                            <th className="table-content">Actions</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -44,28 +71,52 @@ function Users() {
                                 <td className="table-content">{user.id}</td>
                                 <td className="table-content">{user.firstName}</td>
                                 <td className="table-content">{user.lastName}</td>
+                                <td className="table-content">{user.email}</td>
                                 <td className="table-content">{user.gender}</td>
                                 <td className="table-content">{user.dateOfBirth}</td>
-                                <td className="table-content">{user.email}</td>
                                 <td className="table-content">
                                     {user.sports.length === 1
                                         ? user.sports[0]
-                                        : <Dropdown>
-                                            <Dropdown.Toggle variant="success" style={{background: 'none', border: 'none', color: 'black'}}>
-                                            </Dropdown.Toggle>
-                                            <Dropdown.Menu>
-                                                {user.sports.map(sport => (
-                                                    <Dropdown.Item key={sport}>{sport}</Dropdown.Item>
-                                                ))}
-                                            </Dropdown.Menu>
-                                        </Dropdown>
-                                    }
+                                        : (
+                                            <Dropdown>
+                                                <Dropdown.Toggle variant="success" style={{background: 'none', border: 'none', color: 'black'}}>
+                                                </Dropdown.Toggle>
+                                                <Dropdown.Menu>
+                                                    {user.sports.map(sport => (
+                                                        <Dropdown.Item key={sport}>{sport}</Dropdown.Item>
+                                                    ))}
+                                                </Dropdown.Menu>
+                                            </Dropdown>
+                                        )}
+                                </td>
+                                <td className="table-content">
+                                    <div className="button-group">
+                                        <Button className="buttonView" size="sm" onClick={() => handleOpenModal(user)}>View</Button>
+                                        <Button className="buttonDelete" variant="danger" size="sm" onClick={() => handleDelete(user.id)}>Delete</Button>
+                                    </div>
                                 </td>
                             </tr>
                         ))}
                         </tbody>
                     </Table>
                 </div>
+                <div className="empty-space"></div>
+                <Modal show={showModal} onHide={handleCloseModal} centered>
+                    <Modal.Header closeButton>
+                        <Modal.Title>User Details</Modal.Title>
+                    </Modal.Header>
+                    {selectedUser && (
+                        <Modal.Body>
+                            <p><strong>ID:</strong> {selectedUser.id}</p>
+                            <p><strong>First Name:</strong> {selectedUser.firstName}</p>
+                            <p><strong>Last Name:</strong> {selectedUser.lastName}</p>
+                            <p><strong>Email Address:</strong> {selectedUser.email}</p>
+                            <p><strong>Gender:</strong> {selectedUser.gender}</p>
+                            <p><strong>Date of Birth:</strong> {selectedUser.dateOfBirth}</p>
+                            <p><strong>Sports:</strong> {selectedUser.sports.join(', ')}</p>
+                        </Modal.Body>
+                    )}
+                </Modal>
             </div>
         </div>
     );
