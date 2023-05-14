f1f2fe53ede32c33db46de6600d730b54800b629
Nedim <nedim_om@hotmail.com>
2023-05-14 02:10:23 +0200

    Dodao Eminovu bazu

--- backend/sportista/models.py
+++ backend/sportista/models.py
@@ -2,6 +2,45 @@ from django.db import models
 
 # Create your models here.
 
+class Sports(models.Model):
+    name = models.CharField(max_length=255)
+
+
 class Users(models.Model):
-    user_Firstname = models.CharField(max_length=200)
-    user_lastName = models.CharField(max_length=200)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    gender = models.BooleanField()
+    date_of_birth = models.DateField()
+    plays_sports = models.ManyToManyField(Sports)
+
+
+class Renters(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    has_sport = models.ManyToManyField(Sports)
+
+
+class Fields(models.Model):
+    id_rentera = models.ForeignKey(Renters, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    details = models.CharField(max_length=1000)
+    image = models.ImageField()
+    starts = models.TimeField()
+    ends = models.TimeField()
+    is_sport = models.ManyToManyField(Sports)
+
+
+class Teams(models.Model):
+    id_leader = models.ForeignKey(Users, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True)
+
+
+class UserGradesField(models.Model):
+    id_usera = models.ForeignKey(Users, on_delete=models.CASCADE)
+    id_fielda = models.ForeignKey(Fields, on_delete=models.CASCADE)
+    grade = models.IntegerField()
--- backend/sportista/views.py
+++ backend/sportista/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 
-from sportista.models import Users
+#from sportista.models import Users
 
 
 # Create your views here.
@@ -11,9 +11,9 @@ from sportista.models import Users
 def index(request):
     return HttpResponse("To je backend Sporiste hehe")
 
-@api_view(['GET'])
-def getListaUsera(request):
-    list_of_users = Users.objects.all()
-    res = serializers.serialize('json', list_of_users)
+#@api_view(['GET'])
+#def getListaUsera(request):
+ #   list_of_users = Users.objects.all()
+  #  res = serializers.serialize('json', list_of_users)
 
-    return HttpResponse(res, content_type="text/json-comment-filtered")
+   # return HttpResponse(res, content_type="text/json-comment-filtered")
