f394a7995b7be5fab2eb2485c2d066b41787207b
Nedim Omeragić <76268058+NedimOm@users.noreply.github.com>
2023-05-22 22:19:55 +0200

    Delete backend directory

--- backend/backendSportista/authentification.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import redirect
-
-
-def reset_password_confirm(request, uid, token):
-    return redirect('http://localhost:3000/password/reset/confirm/' + uid + "/" + token)
-
-
-def activate_account(request, uid, token):
-    return redirect('http://localhost:3000/activate/' + uid + "/" + token)
