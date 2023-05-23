35d198cecf99f3e2a7e862695baf9a09502b87f5
Alma Babić <117942503+almababic@users.noreply.github.com>
2023-05-23 16:25:54 +0200

    Renter account frontend

--- frontend/src/pages/renter/RenterAccount.js
+++ frontend/src/pages/renter/RenterAccount.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 //components
 import RenterSidebar from "../../components/navigation/RenterSidebar";
+import AccountForRenter from "../../components/renter/AccountForRenter";
 
 function RenterAccount() {
     return (
@@ -9,6 +10,7 @@ function RenterAccount() {
             <RenterSidebar />
             <div style={{ marginLeft: '10px'}}>
                 <h1>Account</h1>
+                <AccountForRenter/>
             </div>
         </div>
     );
