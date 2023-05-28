73ffd954dd8d7d6d2774dd12c8af63f2d606c6d7
Mersiha <mersiboloban@gmail.com>
2023-05-28 23:41:41 +0200

    Tabele sa podacima iz baze prikazane adminu

--- backend/sportista/urls.py
+++ backend/sportista/urls.py
@@ -4,6 +4,8 @@ from sportista import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("admin/renters/getRenters/", views.getRenters, name="getRenters"),
+    path("admin/users/getUsers/", views.getUsers, name="getUsers"),
     path("renter/spremi", views.spremi, name="spremi"),
     path("daj_sportove", views.dajSportove),
     path("sport/<int:params>/", views.getFields, name="getSport"),
