8878ec6e26818e0591e2e3ded4e283410c3424c0
Alma Babić <117942503+almababic@users.noreply.github.com>
2023-06-05 10:19:52 +0200

    Default image for the field in case the renter does not upload any

--- frontend/src/components/renter/FieldFormAction.js
+++ frontend/src/components/renter/FieldFormAction.js
@@ -1,42 +1,38 @@
-import Form from 'react-bootstrap/Form';
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import {connect} from "react-redux";
-import {Navigate} from "react-router-dom";
-import {SERVER_URL} from "../../auth/Consts";
+import { connect } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { SERVER_URL } from "../../auth/Consts";
 import Button from "@material-ui/core/Button";
-import {Box, FormControl, InputLabel, MenuItem, Select, TextareaAutosize, TextField} from "@mui/material";
+import { Box, FormControl, InputLabel, MenuItem, Select, TextareaAutosize, TextField } from "@mui/material";
+import defaultImage from '../../../src/resources/images/favicon.jpg';
 
-function FieldFormAction(props, { user, isAuthenticated } ) {
+function FieldFormAction(props) {
 
-    var objekat;
-    const [sport,setSport] = useState("");
-    const [name,setName] = useState("");
-    const [location,setLocation] = useState("");
-    const [price,setPrice] = useState("");
-    const [images,setImages] = useState(null);
-    const [description,setDescription] = useState("");
+    const [sport, setSport] = useState("");
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [price, setPrice] = useState("");
+    const [images, setImages] = useState(null);
+    const [description, setDescription] = useState("");
     const [hasSports, setHasSports] = useState([]);
     const [gotData, setGotData] = useState(false);
 
-    if(!gotData)
-        axios.get( `${SERVER_URL}/daj_sportove`).then((res) => {
-            setHasSports(res.data)
-            setGotData(true)
-        })
+    if (!gotData)
+        axios.get(`${SERVER_URL}/daj_sportove`).then((res) => {
+            setHasSports(res.data);
+            setGotData(true);
+        });
 
-
-    if(props.user != null){
-        objekat ={
-            user:props.user.id,
-            sport:sport,
-            name:name,
-            price:price,
-            location:location,
-            img:images,
-            description:description
-        }
-    }
+    const objekat = {
+        user: props.user.id,
+        sport: sport,
+        name: name,
+        price: price,
+        location: location,
+        img: images ? Array.from(images) : [defaultImage],
+        description: description,
+    };
 
     function convertImagesToStrings(fileList) {
         const promises = Array.from(fileList).map((file) => {
@@ -54,42 +50,38 @@ function FieldFormAction(props, { user, isAuthenticated } ) {
         return Promise.all(promises);
     }
 
-
     function posalji() {
-        console.log(objekat);
         if (images) {
             convertImagesToStrings(images)
                 .then((imageStrings) => {
-                    axios.post('http://127.0.0.1:8000/renter/spremi', {...objekat, img:imageStrings} )
-                        .then(response => {
+                    axios
+                        .post(`${SERVER_URL}/renter/spremi`, { ...objekat, img: imageStrings })
+                        .then((response) => {
                             console.log(response.data);
-                            // Handle the successful response
                         })
-                        .catch(error => {
+                        .catch((error) => {
                             console.error(error);
-                            // Handle the error
                         });
                 })
                 .catch((error) => {
                     console.error(error);
                 });
         } else {
-            axios.post('http://127.0.0.1:8000/renter/spremi', objekat )
-                .then(response => {
+            axios
+                .post(`${SERVER_URL}/renter/spremi`, objekat)
+                .then((response) => {
                     console.log(response.data);
-                    // Handle the successful response
                 })
-                .catch(error => {
+                .catch((error) => {
                     console.error(error);
-                    // Handle the error
                 });
         }
     }
 
-    function callFuns(){
-        posalji()
-        props.closeModal()
-        setTimeout(props.getF, 330)
+    function callFuns() {
+        posalji();
+        props.closeModal();
+        setTimeout(props.getF, 330);
     }
 
     return (
@@ -102,7 +94,6 @@ function FieldFormAction(props, { user, isAuthenticated } ) {
                         aria-label="Select sport"
                         variant="outlined"
                         onChange={(e) => setSport(e.target.value)}
-
                     >
                         <MenuItem value="">Select sport</MenuItem>
                         {hasSports.map((sport) => (
@@ -147,9 +138,15 @@ function FieldFormAction(props, { user, isAuthenticated } ) {
                     className="custom-input"
                     type="file"
                     id="formBasicImg"
-                    onChange={(e) => setImages(e.target.files) }
+                    onChange={(e) => setImages(e.target.files)}
                 />
             </Box>
+
+            {!images && (
+                <Box mb={1}>
+                    <img src={defaultImage} alt="Default" width="200" height="200" />
+                </Box>
+            )}
 
             <Box mb={3}>
                 <InputLabel htmlFor="textarea">Description</InputLabel>
@@ -171,6 +168,7 @@ function FieldFormAction(props, { user, isAuthenticated } ) {
         </form>
     );
 }
-const mapStateToProps = state => ({isAuthenticated: state.auth.isAuthenticated, user : state.auth.user});
 
-export default connect(mapStateToProps,null)(FieldFormAction);
+const mapStateToProps = (state) => ({ isAuthenticated: state.auth.isAuthenticated, user: state.auth.user });
+
+export default connect(mapStateToProps, null)(FieldFormAction);
